470fb97641bab6063a9f8a4eaf8ea7cb7fe966fe
starbadboy <songtaontu@gmail.com>
2017-02-15 22:34:17 +0800

    init project
    add an empty multi tab project

---
+++ TotoScanner/TotoScanner/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TotoScanner
+//
+//  Created by SONG TAO on 15/02/2017.
+//  Copyright © 2017 SONG TAO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
